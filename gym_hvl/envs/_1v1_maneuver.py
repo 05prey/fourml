e0ad8a95e490134ed572bda0ec0c40840946ef58
emre.fisne <71825088+05prey@users.noreply.github.com>
2020-10-26 14:02:30 +0300

    Update and rename foo_env.py to _1v1_maneuver.py

---
+++ gym_hvl/envs/_1v1_maneuver.py
@@ -0,0 +1,14 @@
+import gym
+
+class DogfightEnv(gym.Env):
+  
+
+  def __init__(self):
+    print("init method of environment")
+  
+  def step(self, action):
+    print("step method of environment")
+  
+  def reset(self):
+    print("reset method of environment")
+  
