28cffb8d905016425964aea8880fe58bcc8d3e67
emre.fisne <71825088+05prey@users.noreply.github.com>
2020-10-26 14:06:26 +0300

    Update main_sac_maneuver.py

--- learning/HHO/algo/1v1_maneuver_discrete/maneuver_models/main_sac_maneuver.py
+++ learning/HHO/algo/1v1_maneuver_discrete/maneuver_models/main_sac_maneuver.py
@@ -1,1 +1,14 @@
+import gym
+import gym_hvl
+
+env = gym.make('dogfight-v0')
+env.init()
+env.reset()
+
+episodes = 5
+for e in range(episodes):
+        
+    action = [0,0,0]
+    env.step(action)
+
 
