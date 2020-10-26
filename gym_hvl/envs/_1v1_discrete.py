97d2070838c77dcd47d5e4248a0ddda755fe8780
emre.fisne <71825088+05prey@users.noreply.github.com>
2020-10-26 14:53:26 +0300

    Update _1v1_discrete.py

--- gym_hvl/envs/_1v1_discrete.py
+++ gym_hvl/envs/_1v1_discrete.py
@@ -1,1 +1,13 @@
+import gym
 
+class DogfightEnv(gym.Env):
+  
+
+  def __init__(self):
+    print("init method of DISCRETE environment")
+  
+  def step(self, action):
+    print("step method of DISCRETE environment")
+  
+  def reset(self):
+    print("reset method of DISCRETE environment")
