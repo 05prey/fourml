9b331ace56e7495e766e6624977ed4e5af1cb20c
emre.fisne <71825088+05prey@users.noreply.github.com>
2020-10-27 11:26:59 +0300

    Update __init__.py

--- environment/gym_hvl/__init__.py
+++ environment/gym_hvl/__init__.py
@@ -2,11 +2,11 @@ from gym.envs.registration import register
 
 register(
     id='dogfight_maneuver-v0',
-    entry_point='gym_hvl.envs.HHO.vs1_1.maneuver:ManeuverDogfightEnv',
+    entry_point='gym_hvl.envs.HHO.vs1_1.maneuver:DogfightEnv',
 )
 
 register(
     id='dogfight_discrete-v0',
-    entry_point='gym_hvl.envs.HHO.vs1_1.discrete:DiscreteDogfightEnv',
+    entry_point='gym_hvl.envs.HHO.vs1_1.discrete:DogfightEnv',
 )
 
