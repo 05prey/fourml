b37efca0c6bbf1790964dd45c0a55de06f098c51
emre.fisne <71825088+05prey@users.noreply.github.com>
2020-11-16 18:17:24 +0300

    Update AutodidacticHHOAgent.py

--- learning/HHO/rule/1v1/AutodidacticHHOAgent.py
+++ learning/HHO/rule/1v1/AutodidacticHHOAgent.py
@@ -7,3 +7,5 @@ print("This repo is in D: drive")
 print("The simulator calls me from a specific directory which is not a git repo.")
 
 print("But there is a symlink redirecting from specific directory to git repo.")
+
+print("hehe ;) Im another user and I added this line.")
