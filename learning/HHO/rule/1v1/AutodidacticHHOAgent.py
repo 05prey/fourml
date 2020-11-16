f30dbdbcf1872b25f8b2c6ce91b550c30f8836a5
Emre Fisne <emre@fisne.com>
2020-11-16 18:15:42 +0300

    SYMLINK

--- learning/HHO/rule/1v1/AutodidacticHHOAgent.py
+++ learning/HHO/rule/1v1/AutodidacticHHOAgent.py
@@ -1,3 +1,9 @@
 import os
 
 print("Im the target rule file in local git repo.")
+
+print("This repo is in D: drive")
+
+print("The simulator calls me from a specific directory which is not a git repo.")
+
+print("But there is a symlink redirecting from specific directory to git repo.")
